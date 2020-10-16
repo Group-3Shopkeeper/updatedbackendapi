664b2496aaf62261594d7609b49a366a782463e2
meanstack4@gmail.com <meanstack4@gmail.com>
2020-10-16 09:38:56 +0530

    project setup

---
+++ src/main/java/com/hardwarevaluewareapi/HardwarevaluewareapiApplication.java
@@ -0,0 +1,13 @@
+package com.hardwarevaluewareapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwarevaluewareapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwarevaluewareapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hardwarevaluewareapi/HardwarevaluewareapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hardwarevaluewareapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwarevaluewareapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
